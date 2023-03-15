eabebe05c6cda75e581fd275674880da1a252a08
Jahdon Ervin <Jahdonervin@gmail.com>
2023-03-14 20:28:52 -0400

    commit tree 7eb82c0a10c98a3714b01ac0f88372393752f81c

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
-
 function Item({ name, category }) {
   const [isInCart, setIsInCart] = useState(false);
-
   function handleAddToCartClick() {
     setIsInCart((isInCart) => !isInCart);
   }
-
   return (
     <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
